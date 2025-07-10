45732336e5aeea0b79b4669267d20d40d3e25eb1
br41b4nt <alexis.braibant@gmail.com>
2025-07-10 11:44:39 +0200

    added bashls to lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -212,6 +212,7 @@ return {
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        bashls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
